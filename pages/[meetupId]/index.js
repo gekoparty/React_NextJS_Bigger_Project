c43aa4de675b65f272728420abd284b21c85b2f9
gekoparty <gekoparty@hotmail.com>
2022-01-11 20:32:39 +0100

    adding mongodb

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,4 +12,42 @@ const MeetupDetails = () => {
   )
 }
 
+export async function getStaticPaths() {
+  return {
+      fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  }
+}
+
+export async function getStaticProps(context) {
+  //fetch data
+  const meetupId = context.params.meetupId
+
+  console.log(meetupId)
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        id: meetupId,
+        title: 'first meetup',
+        address: 'some street, some city',
+        description: 'this is the first meetup',
+      },
+    },
+  }
+}
+
 export default MeetupDetails

87a22d9781b35216a21f1b6349b07142276f4083
gekoparty <gekoparty@hotmail.com>
2022-01-12 19:57:08 +0100

    Requesting Data

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,35 @@
-import MeetupList from '../components/meetups/MeetupList';
+import MeetupList from '../components/meetups/MeetupList'
+import { MongoClient } from 'mongodb'
 
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A first meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    adress: 'Some Adress, 4355 Haugesund',
-    description: 'This is a first meetup',
-  },
-  {
-    id: 'm2',
-    title: 'A second meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    adress: 'Some Adress again, 4355 Haugesund',
-    description: 'This is a second meetup',
-  },
-  {
-    id: 'm3',
-    title: 'A third meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    adress: 'Some Adress somewhere, 4355 Haugesund',
-    description: 'This is a third meetup',
-  },
-]
 
 const Homepage = (props) => {
-
-    
-
   return <MeetupList meetups={props.meetups}></MeetupList>
 }
 
 export async function getStaticProps() {
-    return {
-        props: {
-            meetups: DUMMY_MEETUPS
-        },
-        revalidate: 10
-    };
+  const client = await MongoClient.connect(
+    'mongodb+srv://testuser:holyriders@gekoparty.t4jd6.mongodb.net/meetups?retryWrites=true&w=majority',
+  )
+  const db = client.db()
+
+  const meetupsCollection = db.collection('meetups')
+
+  const meetups = await meetupsCollection.find().toArray()
+
+  client.close()
+
+  return {
+    props: {
+      meetups: meetups.map(meetup=> ({
+          title: meetup.title,
+          address: meetup.address,
+          image: meetup.image,
+          id: meetup._id.toString()
+      })),
+    },
+    revalidate: 10,
+  }
 }
 
 export default Homepage
